9e14a6ce1a7f9af50f1fd6eb39c5d39984f2a938
RobertoRico <rricolaw@gmail.com>
2015-12-09 13:34:38 -0800

    sorting

---
+++ sort/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sortApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sortApp
+ */
+angular.module('sortApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ sort/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sortApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sortApp
+ */
+angular.module('sortApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
